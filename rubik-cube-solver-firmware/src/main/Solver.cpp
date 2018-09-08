66f90254d4fb2796a70e08515e6d27a9bd6d7db0
richard.zhao <zhaorc@gmail.com>
2018-09-08 22:33:51 +0800

    加debug模式从Serial读取数据

--- rubik-cube-solver-firmware/src/main/Solver.cpp
+++ rubik-cube-solver-firmware/src/main/Solver.cpp
@@ -6,6 +6,7 @@
 #define STATE_SOLVE        2
 #define STATE_SOLVE_FINISH       3
 
+bool testMode = false;
 SoftwareSerial bluetooth(10, 11);
 Stepper push_stepper(4, 5, 6400, 30);
 Stepper rotate_stepper(6, 7, 6400, 60);
@@ -13,7 +14,6 @@ Cube *cube;
 char buf[121];
 int data_size = 0;
 int ptr = 0;
-//char* buf_ptr;
 int state = 0;
 
 void readMoves();
@@ -49,38 +49,83 @@ void loop() {
 
 void readMoves() {
     data_size = 0;
-    //char buf[121];
     int data = 0;
     int read_length = 0;
     // first 4 bytes are length
     // followed by data
-    while (bluetooth.available() < 4) {
-        // do nothing
+    ////////
+    if (testMode) {
+        while (Serial.available() < 4) {
+            // do nothing
+        }
+        data = Serial.read();
+        data_size += ((data - '0') * 1000);
+        data = Serial.read();
+        data_size += ((data - '0') * 100);
+        data = Serial.read();
+        data_size += ((data - '0') * 10);
+        data = Serial.read();
+        data_size += (data - '0');
+        //XXX
+        Serial.print("data_size=");
+        Serial.println(data_size);
+        while (true) {
+            if (Serial.available()) {
+                data = Serial.read();
+                buf[read_length++] = data;
+            }
+            if (read_length == data_size) {
+                break;
+            }
+            //Serial.print("read_length=");
+            //Serial.println(read_length);
+        }
+//        while (Serial.available() < data_size) {
+//            //do nothing
+//        }
+//        for (int i = 0; i < data_size; i++) {
+//            data = Serial.read();
+//            buf[read_length++] = data;
+//        }
     }
-    data = bluetooth.read();
-    data_size += ((data - '0') * 1000);
-    data = bluetooth.read();
-    data_size += ((data - '0') * 100);
-    data = bluetooth.read();
-    data_size += ((data - '0') * 10);
-    data = bluetooth.read();
-    data_size += (data - '0');
-    //XXX
-    Serial.print("data_size=");
-    Serial.println(data_size);
-    while (bluetooth.available() < data_size) {
-        //do nothing
-    }
-    for (int i = 0; i < data_size; i++) {
+    else {
+        while (bluetooth.available() < 4) {
+            // do nothing
+        }
         data = bluetooth.read();
-        buf[read_length++] = data;
+        data_size += ((data - '0') * 1000);
+        data = bluetooth.read();
+        data_size += ((data - '0') * 100);
+        data = bluetooth.read();
+        data_size += ((data - '0') * 10);
+        data = bluetooth.read();
+        data_size += (data - '0');
+        //XXX
+        Serial.print("data_size=");
+        Serial.println(data_size);
+        while (true) {
+            if (bluetooth.available()) {
+                data = bluetooth.read();
+                buf[read_length++] = data;
+            }
+            if (read_length == data_size) {
+                break;
+            }
+            //Serial.print("read_length=");
+            //Serial.println(read_length);
+        }
+//        while (bluetooth.available() < data_size) {
+//            //do nothing
+//        }
+//        for (int i = 0; i < data_size; i++) {
+//            data = bluetooth.read();
+//            buf[read_length++] = data;
+//        }
     }
-//    //清除缓冲区
-//    while (bluetooth.available()) {
-//        data = bluetooth.read();
-//        delay(10);
-//    }
     buf[read_length] = '\0';
+    //XXX
+    Serial.print("buf=");
+    Serial.println(buf);
     //buf_ptr = buf;
     state = STATE_SOLVE;
     ptr = 0;
@@ -101,12 +146,18 @@ void solveCube() {
         switch (c2) {
         case '2':
             cube->x2();
+            //XXX
+            Serial.println("x2");
             break;
         case '\'':
             cube->x_();
+            //XXX
+            Serial.println("x'");
             break;
         default:
             cube->x();
+            //XXX
+            Serial.println("x");
             ptr--;
         }
         break;
@@ -114,12 +165,18 @@ void solveCube() {
         switch (c2) {
         case '2':
             cube->y2();
+            //XXX
+            Serial.println("y2");
             break;
         case '\'':
             cube->y_();
+            //XXX
+            Serial.println("y'");
             break;
         default:
             cube->y();
+            //XXX
+            Serial.println("y");
             ptr--;
         }
         break;
@@ -127,12 +184,18 @@ void solveCube() {
         switch (c2) {
         case '2':
             cube->U2();
+            //XXX
+            Serial.println("U2");
             break;
         case '\'':
             cube->U_();
+            //XXX
+            Serial.println("U'");
             break;
         default:
             cube->U();
+            //XXX
+            Serial.println("U");
             ptr--;
         }
         break;
@@ -140,12 +203,18 @@ void solveCube() {
         switch (c2) {
         case '2':
             cube->D2();
+            //XXX
+            Serial.println("D2");
             break;
         case '\'':
             cube->D_();
+            //XXX
+            Serial.println("D'");
             break;
         default:
             cube->D();
+            //XXX
+            Serial.println("D");
             ptr--;
         }
         break;
@@ -153,12 +222,18 @@ void solveCube() {
         switch (c2) {
         case '2':
             cube->L2();
+            //XXX
+            Serial.println("L2");
             break;
         case '\'':
             cube->L_();
+            //XXX
+            Serial.println("L'");
             break;
         default:
             cube->L();
+            //XXX
+            Serial.println("L");
             ptr--;
         }
         break;
@@ -166,12 +241,18 @@ void solveCube() {
         switch (c2) {
         case '2':
             cube->R2();
+            //XXX
+            Serial.println("R2");
             break;
         case '\'':
             cube->R_();
+            //XXX
+            Serial.println("R'");
             break;
         default:
             cube->R();
+            //XXX
+            Serial.println("R");
             ptr--;
         }
         break;
@@ -179,12 +260,18 @@ void solveCube() {
         switch (c2) {
         case '2':
             cube->FF2();
+            //XXX
+            Serial.println("F2");
             break;
         case '\'':
             cube->FF_();
+            //XXX
+            Serial.println("F'");
             break;
         default:
             cube->FF();
+            //XXX
+            Serial.println("F");
             ptr--;
         }
         break;
@@ -192,12 +279,18 @@ void solveCube() {
         switch (c2) {
         case '2':
             cube->B2();
+            //XXX
+            Serial.println("B2");
             break;
         case '\'':
             cube->B_();
+            //XXX
+            Serial.println("B'");
             break;
         default:
             cube->B();
+            //XXX
+            Serial.println("B");
             ptr--;
         }
         break;
