d98b4d9ceb1a931d69ed2b0bf72396f55e7bb63f
richard.zhao <zhaorc@gmail.com>
2018-09-12 06:30:00 +0800

    魔方图像采集和标记的app

--- rubik-cube-solver-controller/app-faceletlabel/src/main/java/tthcc/rubiksolver/solver/faceletlabel/PhotoActivity.java
+++ rubik-cube-solver-controller/app-faceletlabel/src/main/java/tthcc/rubiksolver/solver/faceletlabel/PhotoActivity.java
@@ -25,28 +25,16 @@ import tthcc.rubiksolver.solver.faceletlabel.camera.ProcessingThread;
 
 public class PhotoActivity extends AppCompatActivity implements SurfaceHolder.Callback {
     private final String TAG = PhotoActivity.class.getSimpleName();
-
-    private String[] faceletStrings = new String[6];
-
-    //URLDFB is the kociemba's twophase algorithm's face sequence
-    public static final int MSG_FACE_U = 0;
-    public static final int MSG_FACE_R = 1;
-    public static final int MSG_FACE_F = 2;
-    public static final int MSG_FACE_D = 3;
-    public static final int MSG_FACE_L = 4;
-    public static final int MSG_FACE_B = 5;
-
     public static final int MSG_FACE_DETECT_SUCESS = 6;
+    private static final int MSG_DETECT_NEXT_FACE = 7;
 
     private static final int DefaultPreviewWidth = 1440;
     private static final int DefaultPreviewHeight = 1080;
     private SurfaceHolder surfaceHolder;
     private ProcessingThread processingThread;
     private Handler handler;
-    private TextView movesTextView;
-    private TextView stopwatchTextView;
-
-    private boolean uFaceClickEnabled = false;
+    private TextView sampleNumTextView;
+    private Stopwatch stopwatch = new Stopwatch();
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -59,27 +47,13 @@ public class PhotoActivity extends AppCompatActivity implements SurfaceHolder.Ca
         int width = display.getHeight() * 3 / 4 * this.DefaultPreviewHeight / this.DefaultPreviewWidth;
         layoutParams.width = width;
         linearLayout.setLayoutParams(layoutParams);
-
-        this.findViewById(R.id.face_F).setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                if(uFaceClickEnabled) {
-                    uFaceClickEnabled = false;
-                    processingThread.performDetectFace();
-                }
-            }
-        });
-
-        for(int i = 0; i< faceletStrings.length; i++) {
-            this.faceletStrings[i] = "";
-        }
         this.createTextViewOverlay();
-
         this.surfaceHolder = ((SurfaceView)this.findViewById(R.id.camera_surface_view)).getHolder();
         this.surfaceHolder.addCallback(this);
         this.createHandler();
         this.processingThread = new ProcessingThread(ProcessingThread.class.getSimpleName(), this.surfaceHolder, this.handler, this.DefaultPreviewWidth, this.DefaultPreviewHeight);
         this.processingThread.start();
+        this.stopwatch.start();
     }
 
     @Override
@@ -108,13 +82,13 @@ public class PhotoActivity extends AppCompatActivity implements SurfaceHolder.Ca
 
     @Override
     public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-        if(this.movesTextView != null) {
-            this.movesTextView.setText("");
-            this.movesTextView.setVisibility(View.INVISIBLE);
+        if(this.sampleNumTextView != null) {
+            this.sampleNumTextView.setText("");
+            this.sampleNumTextView.setVisibility(View.INVISIBLE);
         }
-        if(this.stopwatchTextView != null) {
-            this.stopwatchTextView.setText("");
-            this.stopwatchTextView.setVisibility(View.INVISIBLE);
+        if(this.sampleNumTextView != null) {
+            this.sampleNumTextView.setText("");
+            this.sampleNumTextView.setVisibility(View.INVISIBLE);
         }
         this.processingThread.performOpenCamera();
         this.processingThread.performStartCamera();
@@ -124,7 +98,6 @@ public class PhotoActivity extends AppCompatActivity implements SurfaceHolder.Ca
         }
         catch(Exception exp){
         }
-        this.uFaceClickEnabled = false;
         this.processingThread.performDetectFace();
     }
 
@@ -141,26 +114,12 @@ public class PhotoActivity extends AppCompatActivity implements SurfaceHolder.Ca
             @Override
             public void handleMessage(Message msg) {
                 switch(msg.what) {
-                    case PhotoActivity.MSG_FACE_U:
-                        showFacePicture((ImageView)findViewById(R.id.face_U), (Bitmap)msg.obj, 90);//(90 + msg.arg1));
-                        break;
-                    case PhotoActivity.MSG_FACE_F:
-                        showFacePicture((ImageView)findViewById(R.id.face_F), (Bitmap)msg.obj, 90);//(90 + msg.arg1));
-                        break;
-                    case PhotoActivity.MSG_FACE_D:
-                        showFacePicture((ImageView)findViewById(R.id.face_D), (Bitmap)msg.obj, 90);//(90 + msg.arg1));
-                        break;
-                    case PhotoActivity.MSG_FACE_B:
-                        showFacePicture((ImageView)findViewById(R.id.face_B), (Bitmap)msg.obj, -90);//(-90 + msg.arg1));
-                        break;
-                    case PhotoActivity.MSG_FACE_L:
-                        showFacePicture((ImageView)findViewById(R.id.face_L), (Bitmap)msg.obj, 0);//msg.arg1);
-                        break;
-                    case PhotoActivity.MSG_FACE_R:
-                        showFacePicture((ImageView)findViewById(R.id.face_R), (Bitmap)msg.obj, 0);//msg.arg1);
-                        break;
                     case PhotoActivity.MSG_FACE_DETECT_SUCESS:
-                        uFaceClickEnabled = true;
+                        showFacePicture((Bitmap)msg.obj, 90);
+                        break;
+                    case PhotoActivity.MSG_DETECT_NEXT_FACE:
+                        detectNextface();
+                        break;
                     default:
                         super.handleMessage(msg);
                 }
@@ -169,46 +128,79 @@ public class PhotoActivity extends AppCompatActivity implements SurfaceHolder.Ca
     }
 
     /**
-     * @param imageView
      * @param bitmap
      * @param  rotateDegree
      */
-    private void showFacePicture(ImageView imageView, Bitmap bitmap, int rotateDegree) {
-        // 旋转90度
-        if(rotateDegree != 0) {
-            Matrix m = new Matrix();
-            m.setRotate(rotateDegree, (float) bitmap.getWidth(), (float) bitmap.getHeight());
-            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, true);
+    private void showFacePicture(Bitmap bitmap, int rotateDegree) {
+        try {
+            ImageView imageView = this.findViewById(R.id.facelet);
+            // 旋转90度
+            if (rotateDegree != 0) {
+                Matrix m = new Matrix();
+                m.setRotate(rotateDegree, (float) bitmap.getWidth(), (float) bitmap.getHeight());
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, true);
+            }
+            imageView.setImageBitmap(bitmap);
+            this.stopwatch.startWatch();
         }
-        imageView.setImageBitmap(bitmap);
+        catch(Exception exp) {
+            Log.e(TAG, exp.getMessage(), exp);
+        }
+    }
+
+    /**
+     *
+     */
+    private void detectNextface() {
+        ImageView imageView = this.findViewById(R.id.facelet);
+        imageView.setImageBitmap(null);
+        this.processingThread.performDetectFace();
     }
 
     /**
      *
      */
     private void createTextViewOverlay() {
-        //用来显示moves的TextView
-        this.movesTextView = new TextView(this.getApplicationContext());
-        this.movesTextView.setVisibility(View.INVISIBLE);
-        this.movesTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        this.movesTextView.setTextColor(Color.WHITE);
-        this.movesTextView.setTextSize(20);
-        FrameLayout.LayoutParams movesTvLayoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        movesTvLayoutParams.gravity = Gravity.TOP;
-        movesTvLayoutParams.topMargin = 50;
-        movesTvLayoutParams.leftMargin = 50;
-        movesTvLayoutParams.rightMargin = 50;
-        this.addContentView(this.movesTextView, movesTvLayoutParams);
-
-        this.stopwatchTextView = new TextView(this.getApplicationContext());
-        this.stopwatchTextView.setVisibility(View.INVISIBLE);
-        this.stopwatchTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        this.stopwatchTextView.setTextColor(Color.WHITE);
-        this.stopwatchTextView.setGravity(Gravity.CENTER_VERTICAL);
-        this.stopwatchTextView.setTextSize(30);
+        this.sampleNumTextView = new TextView(this.getApplicationContext());
+        this.sampleNumTextView.setVisibility(View.INVISIBLE);
+        this.sampleNumTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        this.sampleNumTextView.setTextColor(Color.WHITE);
+        this.sampleNumTextView.setGravity(Gravity.CENTER_VERTICAL);
+        this.sampleNumTextView.setTextSize(30);
         FrameLayout.LayoutParams stopwatchTvLayoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         stopwatchTvLayoutParams.gravity = Gravity.TOP;
         stopwatchTvLayoutParams.topMargin = 300;
-        this.addContentView(this.stopwatchTextView, stopwatchTvLayoutParams);
+        this.addContentView(this.sampleNumTextView, stopwatchTvLayoutParams);
+    }
+
+    /**
+     *
+     */
+    private class Stopwatch extends Thread {
+        private boolean running = false;
+
+        /**
+         *
+         */
+        public void startWatch() {
+            this.running = true;
+        }
+
+        @Override
+        public void run() {
+            try{
+                while(true) {
+                    if(!this.running) {
+                        Thread.sleep(10);
+                        continue;
+                    }
+                    Thread.sleep(2000);
+                    this.running = false;
+                    handler.sendEmptyMessage(PhotoActivity.MSG_DETECT_NEXT_FACE);
+                }
+            }
+            catch(Exception exp) {
+            }
+        }
     }
 }
--- rubik-cube-solver-controller/app-faceletlabel/src/main/java/tthcc/rubiksolver/solver/faceletlabel/camera/ProcessingThread.java
+++ rubik-cube-solver-controller/app-faceletlabel/src/main/java/tthcc/rubiksolver/solver/faceletlabel/camera/ProcessingThread.java
@@ -17,7 +17,6 @@ import com.catalinjurjiu.rubikdetector.model.RubikFacelet;
 import java.nio.ByteBuffer;
 
 import tthcc.rubiksolver.solver.faceletlabel.PhotoActivity;
-import tthcc.rubiksolver.solver.faceletlabel.bluetooth.BluetoothUtil;
 
 public class ProcessingThread extends HandlerThread implements Camera.PreviewCallback {
 
@@ -42,24 +41,6 @@ public class ProcessingThread extends HandlerThread implements Camera.PreviewCal
     private RubikDetector rubikDetector;
 
     private final Object cleanupLock = new Object();
-    private static final int[] FaceSequance = new int[]{
-            PhotoActivity.MSG_FACE_U,
-            PhotoActivity.MSG_FACE_F,
-            PhotoActivity.MSG_FACE_D,
-            PhotoActivity.MSG_FACE_B,
-            PhotoActivity.MSG_FACE_L,
-            PhotoActivity.MSG_FACE_R
-    };
-    private static final String[] RubikTurnfaceSequance = new String[]{
-            "",
-            "x2",
-            "x",
-            "x",
-            "yx",
-            "x",
-            "xy'x",
-    };
-    private int currentFace = -1;
     private int detectedTimes = 0;
     private boolean faceDetacted = true;
 
@@ -69,14 +50,12 @@ public class ProcessingThread extends HandlerThread implements Camera.PreviewCal
      */
     public ProcessingThread(String name, SurfaceHolder surfaceHolder, Handler frontendHandler, int cameraPreviewWidth, int cameraPreviewHeight) {
         super(name);
-        this.currentFace = -1;
         this.detectedTimes = 0;
         this.faceDetacted = true;
         this.surfaceHolder = surfaceHolder;
         this.frontendHandler = frontendHandler;
         this.cameraPreviewWidth = cameraPreviewWidth;
         this.cameraPreviewHeight = cameraPreviewHeight;
-        BluetoothUtil.getInstance().setProcessingThread(this);
     }
 
     /**
@@ -244,30 +223,8 @@ public class ProcessingThread extends HandlerThread implements Camera.PreviewCal
      *
      */
     private void detectFace() {
-        this.currentFace = this.getNextFace();
-        if (this.currentFace != -1) {
-            this.faceDetacted = false;
-            this.detectedTimes = 0;
-        }
-        else {
-            this.frontendHandler.sendEmptyMessage(PhotoActivity.MSG_FACE_DETECT_SUCESS);
-        }
-//        else {
-//            if (this.solveInProcessing) {
-//                this.frontendHandler.sendEmptyMessage(PhotoActivity.MSG_STOP_WATCH);
-//            } else {
-//                String moves = this.computeMoves();
-//                if (moves == null) {
-//                    Message message = this.frontendHandler.obtainMessage(PhotoActivity.MSG_FACE_DETECT_FAIL, "噢～～～～～");
-//                    message.sendToTarget();
-//                } else {
-//                    Message message = this.frontendHandler.obtainMessage(PhotoActivity.MSG_FACE_DETECT_SUCESS, moves);
-//                    message.sendToTarget();
-//                    this.solveInProcessing = true;
-//                    BluetoothUtil.getInstance().sendMessage(moves.replaceAll(" ", ""));
-//                }
-//            }
-//        }
+        this.faceDetacted = false;
+        this.detectedTimes = 0;
     }
 
     /**
@@ -278,7 +235,7 @@ public class ProcessingThread extends HandlerThread implements Camera.PreviewCal
             return;
         }
         RubikFacelet[][] facelets = this.rubikDetector.findCube(data);
-        if (facelets != null && this.detectedTimes++ > 10) {
+        if (facelets != null && this.detectedTimes++ > 5) {
             this.faceDetacted = true;
             try {
                 Bitmap photoBitmap = Bitmap.createBitmap(this.cameraPreviewWidth, this.cameraPreviewHeight, Bitmap.Config.ARGB_8888);
@@ -289,43 +246,12 @@ public class ProcessingThread extends HandlerThread implements Camera.PreviewCal
                 byteBuffer.rewind();
                 photoBitmap.copyPixelsFromBuffer(byteBuffer);
                 // send to UI
-                Message resultMessage = this.frontendHandler.obtainMessage(this.currentFace, photoBitmap);
+                Message resultMessage = this.frontendHandler.obtainMessage(PhotoActivity.MSG_FACE_DETECT_SUCESS, photoBitmap);
                 resultMessage.sendToTarget();
-                this.rubikTurnToNextFace();
             } catch (Exception exp) {
                 Log.e(TAG, exp.getMessage(), exp);
             }
         }
-    }
-
-    /**
-     *
-     */
-    private void rubikTurnToNextFace() {
-        int nextFace = this.getNextFace();
-        if (nextFace == -1) {
-            BluetoothUtil.getInstance().sendMessage(ProcessingThread.RubikTurnfaceSequance[ProcessingThread.RubikTurnfaceSequance.length - 1]);
-        } else {
-            BluetoothUtil.getInstance().sendMessage(ProcessingThread.RubikTurnfaceSequance[nextFace]);
-        }
-    }
-
-    /**
-     * @return
-     */
-    private int getNextFace() {
-        if (this.currentFace == -1) {
-            return ProcessingThread.FaceSequance[0];
-        }
-        if (this.currentFace == ProcessingThread.FaceSequance[ProcessingThread.FaceSequance.length - 1]) {
-            return -1;
-        }
-        for (int i = 0; i < ProcessingThread.FaceSequance.length - 1; i++) {
-            if (ProcessingThread.FaceSequance[i] == this.currentFace) {
-                return ProcessingThread.FaceSequance[i + 1];
-            }
-        }
-        return -1;
     }
 
     /**
@@ -341,119 +267,4 @@ public class ProcessingThread extends HandlerThread implements Camera.PreviewCal
         }
         return -1;
     }
-
-//    /**
-//     * @param rubikFacelets
-//     */
-//    private void makeupFaceletString(RubikFacelet[][] rubikFacelets) {
-//        int degree = this.getRotateDegree(rubikFacelets);
-//        if (this.currentFace == PhotoActivity.MSG_FACE_U || this.currentFace == PhotoActivity.MSG_FACE_F || this.currentFace == PhotoActivity.MSG_FACE_D) {
-//            //U F D 90度
-//            degree = 90 - degree;
-//        } else if (this.currentFace == PhotoActivity.MSG_FACE_B) {
-//            //B -90度
-//            degree = -90 - degree;
-//        } else {
-//            // L R 不转
-//        }
-//        if (degree == -180) {
-//            degree = 180;
-//        } else if (degree == -270) {
-//            degree = 90;
-//        }
-//        RubikFacelet[][] actualFacelets = this.transposition(rubikFacelets, degree);
-//
-//        //XXX
-//        Log.i(TAG, "currentFace=" + this.currentFace);
-//        Log.i(TAG, "rad=" + (rubikFacelets[0][0]).angle * 180 / Math.PI + ", degree=" + degree);
-//        Log.i(TAG, ">>>> " + rubikFacelets[0][0].color + " " + rubikFacelets[0][1].color + " " + rubikFacelets[0][2].color);
-//        Log.i(TAG, ">>>> " + rubikFacelets[1][0].color + " " + rubikFacelets[1][1].color + " " + rubikFacelets[1][2].color);
-//        Log.i(TAG, ">>>> " + rubikFacelets[2][0].color + " " + rubikFacelets[2][1].color + " " + rubikFacelets[2][2].color);
-//        Log.i(TAG, ">>>>>");
-//        Log.i(TAG, ">>>> " + actualFacelets[0][0].color + " " + actualFacelets[0][1].color + " " + actualFacelets[0][2].color);
-//        Log.i(TAG, ">>>> " + actualFacelets[1][0].color + " " + actualFacelets[1][1].color + " " + actualFacelets[1][2].color);
-//        Log.i(TAG, ">>>> " + actualFacelets[2][0].color + " " + actualFacelets[2][1].color + " " + actualFacelets[2][2].color);
-//
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                this.facelets[this.currentFace] += Facename[actualFacelets[i][j].color];
-//            }
-//        }
-//        //XXX
-//        Log.i(TAG, "facelets[" + this.currentFace + "]=" + this.facelets[this.currentFace]);
-//
-//    }
-
-//    /**
-//     * @param rubikFacelets
-//     * @return
-//     */
-//    private int getRotateDegree(RubikFacelet[][] rubikFacelets) {
-//        float angle = (float) (rubikFacelets[0][0].angle * 180 / Math.PI);
-//        int degree = 0;
-//        if (angle > -110 && angle < -70) {
-//            degree = -90;
-//        } else if (angle > -20 && angle < 20) {
-//            degree = 0;
-//        } else if (angle > 70 && angle < 110) {
-//            degree = 90;
-//        } else if (angle > 160 && angle < 200) {
-//            degree = 180;
-//        }
-//        return degree;
-//    }
-
-//    /**
-//     * @param org
-//     * @param degree
-//     * @return
-//     */
-//    private RubikFacelet[][] transposition(RubikFacelet[][] org, int degree) {
-//        RubikFacelet[][] dst = new RubikFacelet[3][3];
-//        if (degree == -90) {
-//            dst[0][0] = org[0][2];
-//            dst[0][1] = org[1][2];
-//            dst[0][2] = org[2][2];
-//
-//            dst[1][0] = org[0][1];
-//            dst[1][1] = org[1][1];
-//            dst[1][2] = org[2][1];
-//
-//            dst[2][0] = org[0][0];
-//            dst[2][1] = org[1][0];
-//            dst[2][2] = org[2][0];
-//        } else if (degree == 0) {
-//            //
-//            for (int i = 0; i < 3; i++) {
-//                for (int j = 0; j < 3; j++) {
-//                    dst[i][j] = org[i][j];
-//                }
-//            }
-//        } else if (degree == 90) {
-//            dst[0][0] = org[2][0];
-//            dst[0][1] = org[1][0];
-//            dst[0][2] = org[0][0];
-//
-//            dst[1][0] = org[2][1];
-//            dst[1][1] = org[1][1];
-//            dst[1][2] = org[0][1];
-//
-//            dst[2][0] = org[2][2];
-//            dst[2][1] = org[1][2];
-//            dst[2][2] = org[0][2];
-//        } else if (degree == 180) {
-//            dst[0][0] = org[2][2];
-//            dst[0][1] = org[2][1];
-//            dst[0][2] = org[2][0];
-//
-//            dst[1][0] = org[1][2];
-//            dst[1][1] = org[1][1];
-//            dst[1][2] = org[1][0];
-//
-//            dst[2][0] = org[0][2];
-//            dst[2][1] = org[0][1];
-//            dst[2][2] = org[0][0];
-//        }
-//        return dst;
-//    }
 }
